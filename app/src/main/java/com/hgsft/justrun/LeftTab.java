7ce897165b704309b01823fd6c8dae429cf1fc78
ilya <ilya_at_csu@mail.ru>
2017-12-15 19:19:46 +0300

    +add distance choser dialog

--- app/src/main/java/com/hgsft/justrun/LeftTab.java
+++ app/src/main/java/com/hgsft/justrun/LeftTab.java
@@ -2,15 +2,20 @@ package com.hgsft.justrun;
 
 import android.Manifest;
 import android.annotation.SuppressLint;
+import android.content.DialogInterface;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.AlertDialog;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.RadioGroup;
+import android.widget.TextView;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -18,15 +23,108 @@ import com.google.android.gms.maps.MapView;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.model.LatLng;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import info.hoang8f.android.segmented.SegmentedGroup;
+import me.angrybyte.numberpicker.listener.OnValueChangeListener;
+import me.angrybyte.numberpicker.view.ActualNumberPicker;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class LeftTab extends Fragment implements OnMapReadyCallback {
 
+    private View mainView;
     private MapView mapView;
     private GoogleMap map;
+    private Button modeSettings;
+    private SegmentedGroup group;
+    private int selectedMode = R.id.basicMode;
+    private Map<Integer, int[]> modeDialogSettings = new HashMap<Integer, int[]>() {{
+        //Resource id as key, value is:
+        //                             0 - layout id, 1 - title text id, 2 - result suffix value, 3 - result getter(s)?
+        put(Integer.valueOf(R.id.distanceMode), new int[] {R.layout.dialog_number_picker, R.string.distance, R.string.distanceSuffixKM, R.id.actual_picker});
+        //TODO: fill table with all modes
+    }};
+
+
+    View.OnClickListener onClickModeSettings = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            final AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+            final int[] settingsKey = modeDialogSettings.get(Integer.valueOf(selectedMode));
+            int layoutId = settingsKey[0];
+            final View view = LayoutInflater.from(getContext()).inflate(layoutId, null);
+            final ActualNumberPicker valuePicker = (ActualNumberPicker)view.findViewById(settingsKey[3]);
+            final String suffix = getResources().getString(settingsKey[2]);
+            final TextView textView = (TextView)view.findViewById(R.id.textView);
+            valuePicker.setListener(new OnValueChangeListener() {
+                @Override
+                public void onValueChanged(int oldValue, int newValue) {
+                    float value = (float)newValue / 10;
+                    textView.setText(String.format("%.2f %s", value, suffix));
+                    int max = valuePicker.getMaxValue();
+                    int min = valuePicker.getMinValue();
+                    //TODO: it can be better, may be chose another value picker?
+                    int tr = (int)((max - min)*0.2);
+                    if (max - newValue <  tr) {
+                        valuePicker.setMaxValue(max + tr);
+                        valuePicker.setMinValue(min + tr);
+                    } else if (newValue - min < tr && min - tr > 0) {
+                        valuePicker.setMaxValue(max - tr);
+                        valuePicker.setMinValue(min - tr);
+                    }
+                }
+            });
+            //TODO: set current value or default
+
+            int titleId = settingsKey[1];
+            builder.setTitle(titleId)
+                    .setView(view)
+                    .setPositiveButton(R.string.OK, new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            getActivity().runOnUiThread(new Runnable() {
+                                @Override
+                                public void run() {
+                                    //TODO: set value to var and save it to settings
+                                    float value = (float)valuePicker.getValue() / 10;
+                                    modeSettings.setText(String.format("%.2f %s", value, suffix));
+                                }
+                            });
+                        }
+                    })
+                    .setNegativeButton(R.string.CANCELL, new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                        }
+                    })
+                    .create()
+                    .show();
+        }
+    };
+
+    View.OnClickListener onClickSettings = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            //TODO: show settings activity
+        }
+    };
+
+    View.OnClickListener onClickStartRun = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            //TODO: change mode/show running activity?
+        }
+    };
+
+    View.OnClickListener onClickMusicSettings = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            //TODO: open music settings activity
+        }
+    };
 
     public LeftTab() {
         // Required empty public constructor
@@ -35,28 +133,44 @@ public class LeftTab extends Fragment implements OnMapReadyCallback {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_left_tab, container, false);
+        mainView = inflater.inflate(R.layout.fragment_left_tab, container, false);
 
         // Gets the MapView from the XML layout and creates it
-        mapView = (MapView) view.findViewById(R.id.mapView);
+        mapView = (MapView) mainView.findViewById(R.id.mapView);
         mapView.onCreate(savedInstanceState);
 
         mapView.getMapAsync(this);
 
-        SegmentedGroup group = (SegmentedGroup)view.findViewById(R.id.segmented2);
+        group = (SegmentedGroup)mainView.findViewById(R.id.segmented2);
         group.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(RadioGroup group, int checkedId) {
                 onSelectInternalTabBtn(checkedId);
             }
         });
+
+        modeSettings = (Button)mainView.findViewById(R.id.modeSettings);
+        modeSettings.setOnClickListener(onClickModeSettings);
+
+        FloatingActionButton settingsBtn = (FloatingActionButton)mainView.findViewById(R.id.settingsButton);
+        settingsBtn.setOnClickListener(onClickSettings);
+
+        Button startTrainingBtn = (Button) mainView.findViewById(R.id.startTrainingButton);
+        startTrainingBtn.setOnClickListener(onClickStartRun);
+
+        FloatingActionButton musicButton = (FloatingActionButton)mainView.findViewById(R.id.musicButton);
+        musicButton.setOnClickListener(onClickMusicSettings);
+
         // Inflate the layout for this fragment
-        return view;
+        return mainView;
 
     }
 
     private void onSelectInternalTabBtn(int index) {
-        //TODO: change view/mode
+        selectedMode = index;
+        int visibility = selectedMode == R.id.basicMode ? View.INVISIBLE : View.VISIBLE;
+        modeSettings.setVisibility(visibility);
+        //TODO: update button text (read from settings)
     }
 
     @Override
