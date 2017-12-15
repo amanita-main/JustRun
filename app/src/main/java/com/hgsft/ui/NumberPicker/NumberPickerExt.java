219d4bc82608b250826522435645b508cfd566e6
Motylev.Ilya <motylev.ia@gmail.com>
2017-12-16 01:47:12 +0300

    +add NumberPickerExt: added TouchEventListener to ActualNumberPicker

---
+++ app/src/main/java/com/hgsft/ui/NumberPicker/NumberPickerExt.java
@@ -0,0 +1,52 @@
+package com.hgsft.ui.NumberPicker;
+
+import android.content.Context;
+import android.os.Handler;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+
+import me.angrybyte.numberpicker.view.ActualNumberPicker;
+
+/**
+ * Created by Ilya on 16.12.2017.
+ */
+
+public class NumberPickerExt extends ActualNumberPicker {
+
+    private Handler mEventsHandler = new Handler();
+    private OnTouchEventListnener mTouchListener;
+
+    public NumberPickerExt(Context context) {
+        super(context);
+    }
+
+    public NumberPickerExt(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public NumberPickerExt(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    public NumberPickerExt(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+    }
+
+    public void setOnTouchEventListnener(OnTouchEventListnener listener) {
+        mTouchListener = listener;
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        final int action = event.getAction();
+        mEventsHandler.post(new Runnable() {
+            @Override
+            public void run() {
+                if (mTouchListener != null) {
+                    mTouchListener.OnTouchEvent(action);
+                }
+            }
+        });
+        return super.onTouchEvent(event);
+    }
+}
---
+++ app/src/main/java/com/hgsft/ui/NumberPicker/OnTouchEventListnener.java
@@ -0,0 +1,11 @@
+package com.hgsft.ui.NumberPicker;
+
+import android.view.MotionEvent;
+
+/**
+ * Created by Ilya on 16.12.2017.
+ */
+
+public interface OnTouchEventListnener {
+    void OnTouchEvent(int eventAction);
+}
